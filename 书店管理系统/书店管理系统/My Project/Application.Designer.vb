aeea29d2b2387f1b6660b034668d1fc3a2a431c6
yxy-start <15282716136@163.com>
2021-06-27 16:38:13 +0800

    'yxy'

--- 书店管理系统/书店管理系统/My Project/Application.Designer.vb
+++ 书店管理系统/书店管理系统/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.书店管理系统.UserLogin
+            Me.MainForm = Global.书店管理系统.Manage_Consumer
         End Sub
     End Class
 End Namespace
